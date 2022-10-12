6493647838e13c9dad530e120cb5d29af8c62ae0
jordan smith <jordan.smith6035@gmail.com>
2022-10-12 16:20:45 -0600

    Complete meal test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price, 
+    type: type,
+  }
+  return menuItem
+}
+
+function addIngredients(toppings, ingredientsToAdd) {
+  if (!ingredientsToAdd.includes(toppings))
+    ingredientsToAdd.push(toppings)
+}
+
+function formatPrice(price) {
+ return `$${price}`
+}
+
+function decreasePrice(price) {
+var salePrice = price * .10
+var finalPrice = price - salePrice
+return finalPrice
+}
+
+function createRecipe (title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
