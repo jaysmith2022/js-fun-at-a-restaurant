6053e65258415bc089ebbfb217011d5713a424de
jordan smith <jordan.smith6035@gmail.com>
2022-10-12 17:55:36 -0600

    complete orders test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(orders, deliveryOrders) {
+  // deliveryOrders.push(orders)
+  if (deliveryOrders.length < 3) {
+   deliveryOrders.push(orders)
+  }
+}
+
+function refundOrder(orderNum, deliveryOrder) {
+ if (orderNum === 1657) {
+  deliveryOrder.shift(orderNum)
+ } else {
+  if (orderNum === 2893) {
+    deliveryOrder.splice(1,1)
+  }
+ }
+}
+
+ function listItems(deliveryOrder) {
+var sandwichType = []
+  for (var i = 0; i < deliveryOrder.length;i++) {
+    sandwichType.push(deliveryOrder[i].item)
+}
+return sandwichType.join(`, `)
+ }
+
+ function searchOrder(deliveryOrders, food) {
+  var onList = false
+  for (var i = 0; i < deliveryOrders.length;i++) {
+    if (deliveryOrders[i].item.includes(food)) {
+      onList = true
+    } 
+  }
+  return onList
+ }
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
